765c2d0508e2efdd576d0950b9a575e8f0981851
Omkar Shinde <shindeomkar7613@gmail.com>
2026-01-05 19:38:33 +0530

    implement character movement and jump

--- Character/player.gd
+++ Character/player.gd
@@ -1,25 +1,38 @@
 extends CharacterBody2D
 
+@export var speed : float = 150.0
+@export var jump_velocity : float = -150.0
+@export var double_jumped : float = -100
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var has_double_jumped : bool = false
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		has_double_jumped = false
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") :
+		if is_on_floor():
+			#normal jump
+			velocity.y = jump_velocity
+			has_double_jumped = false
+		elif not has_double_jumped:
+			#double jump in air
+			velocity.y = jump_velocity
+			has_double_jumped = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
